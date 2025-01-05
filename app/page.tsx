7aee7aff1f8ab5720bc8bbfa4006cbd05203ab23
skar777123 <sanskargaikwad206@gmail.com>
2025-01-06 01:46:03 +0530

    changes 17.9

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { Background } from "./components/Background";
 import { BackgroundBeams } from "./components/ui/background-beams";
 
@@ -5,6 +6,12 @@ export default function Home() {
   return (
     <div>
       <main>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
         <Background />
         <BackgroundBeams className="absolute" />
       </main>
